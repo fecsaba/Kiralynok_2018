b0c64fa1066ac3568409408c7c4fa2ad0727b2e7
t103-17 <t103-17@jedlik.helyi>
2018-01-23 17:45:45 +0100

    Add project files.

---
+++ Kiralynok.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Kiralynok
+{
+    class Kiralynok
+    {
+        static void Main(string[] args)
+        {
+            int r = 7;
+            int c = 9;
+            Console.WriteLine("4. feladat: Az üres tábla:");
+            Tábla tábla = new Tábla('#', r, c);
+            tábla.Megjelenít();
+
+            Console.WriteLine("\n6. feladat: A feltöltött tábla:");
+            tábla.Elhelyez(8);
+            tábla.Megjelenít();
+
+            Console.WriteLine("\n9. feladat: Üres oszlopok és sorok száma");
+            Console.WriteLine($"Oszlopok: {tábla.ÜresOszlopokSzáma}");
+            Console.WriteLine($"Sorok: {tábla.ÜresSorokSzáma}");
+
+            //10. feladat
+            const string fájlNeve = "tablak64.txt";
+            Console.WriteLine($"10. feladat: {fájlNeve}");
+            if (File.Exists(fájlNeve)) File.Delete(fájlNeve);
+            StreamWriter sw = new StreamWriter(fájlNeve);
+            for (int i = 1; i < 65; i++) // 1..64
+            {
+                Tábla aktTábla = new Tábla('*', r, c);
+                aktTábla.Elhelyez(i); //1, 2, 3, .., 64 db 'K'-t
+                aktTábla.FájlbaÍr(sw);
+            }
+            sw.Close(); // Stream (file) lezárása
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tábla.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class Tábla // 1. feladat
+    {
+        // Privát mezők: 
+        char[,] T; // 2. feladat
+        char ÜresCella;  // 2. feladat
+        static Random R = new Random();
+        int Row;
+        int Col;
+
+        public Tábla(char üresCella, int row, int col) // 3. feladat ctor TAB-TAB -> konstruktort hoz létre
+        {
+            T = new char[row, col]; // 3.a
+            ÜresCella = üresCella; // 3.b
+            Row = row;
+            Col = col;
+            for (int sor = 0; sor < T.GetLength(0); sor++) // 3.c
+            {
+                for (int oszlop = 0; oszlop < T.GetLength(1); oszlop++)
+                {
+                    T[sor, oszlop] = ÜresCella;
+                }
+            }
+        }
+
+        public void Megjelenít()
+        {
+            for (int sor = 0; sor < T.GetLength(0); sor++)
+            {
+                for (int oszlop = 0; oszlop < T.GetLength(1); oszlop++)
+                {
+                    // Console.Write("{0,-2}", T[sor, oszlop]);
+                    Console.Write($"{T[sor, oszlop],-2}");
+                }
+                Console.WriteLine(); // soremelés a sor kiírása után
+            }
+        }
+
+        public void Elhelyez(int N) // 5. feladat
+        {
+            for (int i = 0; i < N; )
+            {
+                int sor = R.Next(0, Row);
+                int oszlop = R.Next(0, Col);
+                if (T[sor,oszlop] == ÜresCella)
+                {
+                    T[sor, oszlop] = 'K';
+                    i++;
+                }
+            }
+        }
+
+        public bool ÜresSor(int sor) // 7. feladat
+        {
+            bool üres = true;
+            for (int oszlop = 0; oszlop < T.GetLength(1); oszlop++)
+            {
+                if (T[sor, oszlop] == 'K')
+                {
+                    üres = false;
+                    break;
+                }
+            }
+            return üres;
+        }
+
+        public bool ÜresOszlop(int oszlop) // 7. feladat
+        {
+            bool üres = true;
+            for (int sor = 0; sor < T.GetLength(0); sor++)
+            {
+                if (T[sor, oszlop] == 'K')
+                {
+                    üres = false;
+                    break;
+                }
+            }
+            return üres;
+        }
+
+        public int ÜresSorokSzáma // 8. feladat
+        {
+            get
+            {
+                int db = 0;
+                for (int sor = 0; sor < T.GetLength(0); sor++)
+                {
+                    if (ÜresSor(sor)) db++;
+                }
+                return db;
+            }
+        }
+
+        public int ÜresOszlopokSzáma // 8. feladat
+        {
+            get
+            {
+                int db = 0;
+                for (int oszlop = 0; oszlop < T.GetLength(1); oszlop++)
+                {
+                    if (ÜresOszlop(oszlop)) db++;
+                }
+                return db;
+            }
+        }
+
+        public void FájlbaÍr(StreamWriter sw)
+        {
+            for (int sor = 0; sor < T.GetLength(0); sor++)
+            {
+                for (int oszlop = 0; oszlop < T.GetLength(1); oszlop++)
+                {
+                    sw.Write($"{T[sor, oszlop]}");
+                }
+                sw.WriteLine(); // új sor a fájban
+            }
+            sw.WriteLine(); // új sor a tábla "után"
+        }
+    }
+}
